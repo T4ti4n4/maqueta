61b3a53e26e8e52469663932675f9b211a0aa3c5
T4ti4n4 <rooney2001.TBG@gmail.com>
2024-10-02 12:36:26 -0500

    Cambios Generales en el proyecto
    Cambio en la página principal. Se asigna una ruta HOME.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import AboutSection from './components/AboutSection';
+import MainSection from './components/MainSection';
+import Home from './components/Home';
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <AboutSection />
+    </div>
+  );
+}
+
+function RouteHome() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Page />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 function App() {
   return (
-
+    <div>
+      <RouteHome />
+    </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h1>Un sitio para descubrir decoraciones para tu hogar</h1>
+                <p>
+                    Disfruta de diseños personalizados, productos de diseño y decoración exclusivos para tu hogar.
+                    Encuentra lo que buscas y compra lo que deseas!
+                
+                </p>
+                <a href="/home" className="btn">Mirar ahora</a>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
