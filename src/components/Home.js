cfdd70de1ba99516eb4830e006cbc0fcde657c2c
Alexa1302 <christinesanchez0903@gmail.com>
2024-10-02 18:14:35 -0500

    Home.CSS
    Se agrega CSS al Home.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
     return (
-        <section className="hero">
-            <div className="hero-content">
+        <section className={styles.hero}>
+            <div className={styles.heroContent}>
                 <h1>Un sitio para descubrir decoraciones para tu hogar</h1>
                 <p>
                     Disfruta de diseños personalizados, productos de diseño y decoración exclusivos para tu hogar.
                     Encuentra lo que buscas y compra lo que deseas!
                 
                 </p>
-                <a href="/home" className="btn">Mirar ahora</a>
+                <a href="/home" className={styles.btn}>Mirar ahora</a>
             </div>
         </section>
     );
